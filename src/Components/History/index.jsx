f84a9b7431c6e5b9ef92b68268e4c2fa34a86bed
Nate Brown <nathan.d.brown@gmail.com>
2023-05-11 23:43:13 -0700

    use reducer

---
+++ src/Components/History/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// results functional component
+function History(props) {
+  return (
+    // <section>
+    //   <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+    // </section>
+  );
+}
+
+// leave class component version for reference
+
+// class Results extends React.Component {
+//   render() {
+//     return (
+//       <section>
+//         <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+//       </section>
+//     );
+//   }
+// }
+
+export default History;
