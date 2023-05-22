4c8bd6793035b8b06fac47bc4d44461b05d63ea2
Nate Brown <nathan.d.brown@gmail.com>
2023-05-21 21:52:29 -0700

    returns API data in pretty JSON using fetch, tested with Pokemon API

--- src/App.jsx
+++ src/App.jsx
@@ -13,43 +13,20 @@ function App() {
   const [requestParams, setRequestParams ] = useState({});
 
   // copy this helper function from class component version
-  const callApi = (requestParams) => {
-    // mock output
-    // const data = {
-    //   count: 2,
-    //   results: [
-    //     { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-    //     { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-    //   ],
-    // };
-    const data = JSON.stringify({
-      "abilities": [
-        {
-          "ability": {
-            "name": "limber",
-            "url": "https://pokeapi.co/api/v2/ability/7/"
-          },
-          "is_hidden": false,
-          "slot": 1
-        },
-        {
-          "ability": {
-            "name": "imposter",
-            "url": "https://pokeapi.co/api/v2/ability/150/"
-          },
-          "is_hidden": true,
-          "slot": 3
-        }
-      ],
-      "base_experience": 101,
-      "forms": [
-        {
-          "name": "ditto",
-          "url": "https://pokeapi.co/api/v2/pokemon-form/132/"
-        }
-      ]
-    });
+  const callApi = async (requestParams) => {
     
+    console.log(requestParams);
+    console.log(requestParams.url);
+    console.log(requestParams.method);
+
+    let fetchRequest = await fetch(requestParams.url, {
+      method: requestParams.method
+    });
+
+    console.log(fetchRequest);
+    let fetchResults = await fetchRequest.json();
+    console.log(fetchResults);
+    let data = JSON.stringify(fetchResults);
     setData(data);
     setRequestParams(requestParams);
   }
